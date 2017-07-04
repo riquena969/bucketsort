b04d71903751cfa2879f54d9e1eb53908226d16f
riquena969 <riquena969@gmail.com>
2017-07-03 21:18:15 -0300

    Alteração no layout

--- recursivo.js
+++ recursivo.js
@@ -1,7 +1,7 @@
-function bucketsort(array, quantidadeBaldes) {
+function bucketsort(array, quantidadeBaldes, espacoEsquerda) {
     console.log(array.join());
     // Escrevendo na tela os elementos que chegaram para serem ordenados
-    $('#ordenados').append('<li class="list-group-item">Array que chegou -> ' + array.join(', ') + '</li>');
+    $('#ordenados').append('<li class="list-group-item list-group-item-danger" style="margin-left: ' + espacoEsquerda + 'px">Array que chegou -> ' + array.join(', ') + '</li>');
 
     // Descobrindo o menor e maior elementos
     var menor = array[0];
@@ -12,7 +12,7 @@ function bucketsort(array, quantidadeBaldes) {
     }
 
     // Descobrindo abrangência do balde. (De onde até onde ele vai)
-    abrangencia = (maior - menor) / quantidadeBaldes;
+    var abrangencia = (maior - menor) / quantidadeBaldes;
 
     // Criando baldes
     var buckets = [];
@@ -28,20 +28,18 @@ function bucketsort(array, quantidadeBaldes) {
     }
 
     // Exibindo baldes criados
-    $('#ordenados').append('<li class="list-group-item">Baldes Gerados:<br></li>');
+    $('#ordenados').append('<li class="list-group-item" style="margin-left: ' + espacoEsquerda + 'px">Baldes Gerados:</li>');
     for (var i = 0; i < buckets.length; i++) {
-        $('#ordenados li').last().append('Balde ' + (i + 1) + ' (' + Math.round((menor + (i * abrangencia)) * 100)/100 + ' - ' + Math.round((menor + ((i + 1) * abrangencia)) * 100)/100 + ') - ' + buckets[i].join(', ') + '<br>');
-    }
+        $('#ordenados').append('<li class="list-group-item list-group-item-info" style="margin-left: ' + espacoEsquerda + 'px">Balde ' + (i + 1) + ' (' + Math.round((menor + (i * abrangencia)) * 100)/100 + ' - ' + Math.round((menor + ((i + 1) * abrangencia)) * 100)/100 + ') - ' + buckets[i].join(', ') + '</li>');
 
-    // Verifica se os baldes precisam ser ordenados antes de retornar..
-    for (var i = 0; i < buckets.length; i++) {
+        // Verifica se os baldes precisam ser ordenados antes de retornar..
         elementos = buckets[i];
         ordenar = false;
         for (var j = 1; j < elementos.length; j++) {
             ordenar = elementos[j-1] > elementos[j] ? true : ordenar;
         }
         if (ordenar) {
-            buckets[i] = bucketsort(elementos, quantidadeBaldes);
+            buckets[i] = bucketsort(elementos, quantidadeBaldes, espacoEsquerda + 30);
         }
     }
 
@@ -54,6 +52,9 @@ function bucketsort(array, quantidadeBaldes) {
         }
     }
 
+    // Escrevendo na tela o array ordenado
+    $('#ordenados').append('<li class="list-group-item list-group-item-success" style="margin-left: ' + espacoEsquerda + 'px">Array ordenado: ' + array.join(', ') + '</li>');
+
     // Retornando a array ordenada
     return array;
 }
@@ -63,7 +64,7 @@ $(document).ready(adicionarElemento);
 
 function adicionarElemento() {
     $('.itens').append('\
-        <div class="col-xs-2">\n\
+        <div class="col-xs-3">\n\
             <div class="form-group">\n\
                 <input type="number" class="form-control pontuacao" placeholder="Número" value="' + Math.floor(Math.random() * 21) + '">\n\
             </div>\n\
@@ -73,7 +74,7 @@ function adicionarElemento() {
 function ordenarElementos() {
     $('#ordenados').html('');
 
-    if ($('.itens .col-xs-2').length == 0) {
+    if ($('.itens .col-xs-3').length == 0) {
         alert('Você não tem elementos para ordenar!');
     }
 
@@ -82,12 +83,11 @@ function ordenarElementos() {
         itens.push(parseFloat($(item).val()));
     });
 
-    arrayOrdenado = bucketsort(itens, 5);
-    $('#ordenados').append('<li class="list-group-item active">Array ordenado: ' + arrayOrdenado.join(', ') + '</li>');
+    arrayOrdenado = bucketsort(itens, 5, 0);
 }
 
 $('.itens').on('dblclick', 'input[type=number]', function() {
-    $(this).closest('.col-xs-2').fadeOut(500, function() {
+    $(this).closest('.col-xs-3').fadeOut(500, function() {
         $(this).remove();
     });
 });
